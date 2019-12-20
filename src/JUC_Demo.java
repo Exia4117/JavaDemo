e71298b891a25781b093bd7c5dc8ae389910526a
Exia4117 <593663174.bz@gmail.com>
2019-12-20 11:05:45 +0800

    volatile noAtomicity

--- src/JUC_Demo.java
+++ src/JUC_Demo.java
@@ -6,14 +6,38 @@ class Obj {
     public void ADDTO60() {
         number = 60;
     }
+
+    public void ADDPlusPlus() {
+        number++;
+    }
 }
 
 public class JUC_Demo {
 
     public static void main(String[] args) {
-        seeOKByVolatile();
+//        seeOKByVolatile();
+        noAtomicity();
     }
 
+    //volatile无法保证原子性
+    public static void noAtomicity() {
+        Obj demo = new Obj();
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    demo.ADDPlusPlus();
+                }
+            }, String.valueOf(i)).start();
+
+        }
+        //如果当前活跃线程数 > 2（主线程和gc线程），表示还没有算完，将CPU让给计算线程，直至算完
+        while (Thread.activeCount() > 2){
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName() + "\tfinally the number is \t" + demo.number);
+    }
+
+    //volatile可以保证可见性，及时通知其他线程，主内存的变量已经被修改
     public static void seeOKByVolatile() {
         Obj demo = new Obj();
         new Thread(() -> {
